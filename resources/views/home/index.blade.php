1ed68409373358c06313d84d7619397ba379850f
bmarsilio <bruno.marsilio@hotmail.com>
2015-10-20 00:30:36 -0200

    adicionado arquivo de traducao do datatable

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,4 +4,26 @@
 
     @extends('layout.partial.navbar')
 
+    <div class="table-responsive container">
+        <table class="table table-striped" id="table">
+            <thead>
+                <tr>
+                    <th>Data</th>
+                    <th>Hora</th>
+                    <th>Evento</th>
+                    <th></th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>01/01/2015</td>
+                    <td>18:00</td>
+                    <td>Palesta X</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
 @endsection
